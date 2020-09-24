1fb4c33daf61b9a71353630964ea431d3880f7ad
inzimam.islam <inzimam.islam@timesinternet.in>
2020-09-24 16:42:27 +0530

    name updated

--- behavioral/iterator/CollectionofNames.java
+++ behavioral/iterator/CollectionofNames.java
@@ -6,7 +6,7 @@ public class CollectionofNames implements Container {
 
     public int age[] = {26, 25, 24, 25, 23};
 
-    public String address[] = {"New Delhi", "Gaziabad", "Rishi Kumar", "Rahul Mehta", "Hemant Mishra"};
+    public String address[] = {"DEV","New Delhi", "Gaziabad", "Rishi Kumar", "Rahul Mehta", "Hemant Mishra"};
 
     public String designation[] = {"Java Programmer and Content Writer", "CEO", "Programmer and Web Designer", "Manager", ".Net Trainer"};
 
